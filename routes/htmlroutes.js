c09abd4f2ae0a6e448e74d31f291c4ae5b5bf09d
Allana Anderson <allana.anderson17@gmail.com>
2020-07-26 12:00:51 -0600

    added html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,5 +1,18 @@
-// * The following HTML routes should be created:
 
-//   * GET `/notes` - Should return the `notes.html` file.
 
-//   * GET `*` - Should return the `index.html` file
+//    GET `/notes` - Should return the `notes.html` file.
+
+//    GET `*` - Should return the `index.html` file
+
+var path = require("path");
+
+module.exports = function(app){
+   
+    app.get("/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "../notes.html"));
+    });
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../index.html"));
+    });
+}
